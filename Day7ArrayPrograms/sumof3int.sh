9e852cda875149609d1ea75d7ad05769bef0166b
chaitra845 <chaitrachytra@gmail.com>
2021-06-02 22:05:30 +0530

    ShellScriptPrograms

--- Day7ArrayPrograms/sumof3int.sh
+++ Day7ArrayPrograms/sumof3int.sh
@@ -1,23 +1,16 @@
 # sum of 3 integer
 #!/bin/bash
-read -p "Enter 1st number: " fno
-read -p "Enter 2nd number: " sno
-read -p "Enter 3rd number: " tno
-
-counter=0
-arr[((counter++))]=$fno
-arr[((counter++))]=$sno
-arr[((counter++))]=$tno
-
-echo "Array: ${arr[@]}"
-sum=0
-for num in ${arr[@]}
+cnt=0
+echo "here iam generating range of 3 random numbers and printing the 10 numbers whoes sum is zero"
+while [ $cnt -le 9 ]
 do
-	sum=$((sum+num))	
+   a=$(($RANDOM%10-5))
+   b=$(($RANDOM%10-5))
+   c=$(($RANDOM%10-5))
+
+   if [ $(($a+$b+$c)) -eq 0 ]
+   then
+      ((cnt++))
+      echo $cnt". sum of "$a","$b "and" $c "= 0"
+   fi
 done
-if [ $sum -eq 0 ]
-then
-	echo "Sum of $fno $sno $tno is $sum"
-else
-	echo "Sum of $fno $sno $tno is not zero"
-fi
